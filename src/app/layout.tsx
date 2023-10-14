9b1f3e10c27662a3637678b2b2a9d7bd78fb3dd3
halomanlodestar <kunalranarj2005@gmail.com>
2023-10-14 10:45:59 +0530

    Added FavIcon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="/favicon.png" />
+			</head>
 			<body className={font.className}>
 				<GlobalThemeProvider>
 					<Navbar />
